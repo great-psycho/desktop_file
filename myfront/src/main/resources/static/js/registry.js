a0caf5be68183619b7515fc54eabbc31702d9e0e
Yu_Jehyeop <fbwpguq@gmail.com>
2022-02-10 11:45:33 +0900

    [update] ppt파일 수정 및 추가 작업분

--- myfront/src/main/resources/static/js/registry.js
+++ myfront/src/main/resources/static/js/registry.js
@@ -1,26 +1,24 @@
 $(document).ready(function(){
-    $("#default-public-button").show();
-    $("#default-private-button").hide();
-    $("#public-button").hide();
-    $("#private-button").hide();
+    $(".container").fadeIn(1000);
+    $(".menu").fadeIn(1000);
+    $(".paging_button").fadeIn(1000);
 });
+function saveArticle() {
+    let form_data = new FormData()
+    form_data.append("c1-title-content", $("#h1").val())
+    form_data.append("c1-content-content", $("#h2").val())
 
-function choice_button() {
-    $("#default-public-button").hide();
-    $("#default-private-button").hide();
-    $("#public-button").show();
-    $("#private-button").show();
-}
-function public_check() {
-    $("#default-public-button").show();
-    $("#default-private-button").hide();
-    $("#public-button").hide();
-    $("#private-button").hide();
-}
+    $.ajax({
+        type: "POST",
+        url: `/registry`,
+        data: form_data,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            alert("성공적으로 업로드 되었습니다.");
+            // sessionStorage.setItem("image_idx", response['idx']);
 
-function private_check() {
-    $("#default-public-button").hide();
-    $("#default-private-button").show();
-    $("#public-button").hide();
-    $("#private-button").hide();
+            location.href = "space.html"; // 페이지 변환
+        }
+    });
 }
